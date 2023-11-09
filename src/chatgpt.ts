fd041c66f964d4a733669614c9171a008654ff0c
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-11-09 14:12:41 -0500

    update package.json and tsconfig

--- src/chatgpt.ts
+++ src/chatgpt.ts
@@ -1,55 +1,76 @@
-//import Configuration from 'openai';
-const { OpenAIApi, Configuration } = require('openai');
-//import ChatCompletionRequestMessage from 'openai';
-import * as dotenv from 'dotenv';
-
+import OpenAI from 'openai'; //Open AI SDK 4
+import dotenv from 'dotenv';
 // Load OpenAI API key from environment variable
 dotenv.config();
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+
+const createAssistantAndThread = async () => {
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+      });
 
 
-const assistant = await openai.beta.assistants.create({
-  name: "Math Tutor",
-  instructions: "You are a personal math tutor. Write and run code to answer math questions.",
-  tools: [{ type: "code_interpreter" }],
-  model: "gpt-4-1106-preview"
-});
+    const assistant = await openai.beta.assistants.create({
+      name: "Math Tutor",
+      instructions: "You are a personal math tutor. Write and run code to answer math questions.",
+      tools: [{ type: "code_interpreter" }],
+      model: "gpt-4-1106-preview"
+    });
+
+    const thread = await openai.beta.threads.create(assistant);
+
+    return { assistant, thread };
+  };
+
+  const createAndSendMessage = async (thread) => {
+
+    const message = await openai.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: "I need to solve the equation `3x + 11 = 14`. Can you help me?"
+    });
+
+    return message;
+  };
+
+  const createAndRunAssistant = async (thread, assistant) => {
+
+    const run = await openai.beta.threads.runs.create(thread.id, {
+      assistant_id: assistant.id,
+      instructions: "Please address the user as Jane Doe. The user has a premium account."
+    });
+
+    return run;
+  };
+
+  const retrieveRun = async (thread, runId) => {
 
 
-const thread = await openai.beta.threads.create(assistant);
+    const check = await openai.beta.threads.runs.retrieve(thread.id, runId);
 
+    return check;
+  };
 
-const message = await openai.beta.threads.messages.create(
-  thread.id,
-  {
-    role: "user",
-    content: "I need to solve the equation `3x + 11 = 14`. Can you help me?"
-  }
-);
+  const listMessages = async (thread) => {
 
-console.log("message ", message);
+    const messages = await openai.beta.threads.messages.list(thread.id);
 
-const run = await openai.beta.threads.runs.create(
-  thread.id,
-  {
-    assistant_id: assistant.id,
-    instructions: "Please address the user as Jane Doe. The user has a premium account."
-  }
-);
-console.log("run: " + run)
+    return messages;
+  };
 
-const check = await openai.beta.threads.runs.retrieve(
-  thread.id,
-  run.id
-);
+  (async () => {
+    try {
+      const { assistant, thread } = await createAssistantAndThread();
+      const message = await createAndSendMessage(thread);
+      console.log("message: ", message);
 
-console.log("check: " + check);
+      const run = await createAndRunAssistant(thread, assistant);
+      console.log("run: ", run);
 
-const messages = await openai.beta.threads.messages.list(
-  thread.id
-);
+      const check = await retrieveRun(thread, run.id);
+      console.log("check: ", check);
 
-console.log ("messages: " + messages);
+      const messages = await listMessages(thread);
+      console.log("messages: ", messages);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  })();
